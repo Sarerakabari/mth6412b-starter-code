22c478cc0ff79f871256af4240f31d062c9f9389
Ando Rakotonandrasana <ando-sitraka-2.rakotonandrasana@polymtl.ca>
2024-09-23 10:37:28 -0400

    ajout Constructor

--- src/phase1/comp_connexe.jl
+++ src/phase1/comp_connexe.jl
@@ -4,22 +4,11 @@ include("graph.jl")
 
 """Type abstrait dont d'autres types d'arret dériveront."""
 abstract type Abstractcomp_conx{T} end
-"""Type représentant les aretes d'un graphe.
 
-Exemple:
-
-a        noeud1 = Node("James", 12)
-        noeud2= Node("Kirk",14)
-        
-        edge=Edge("(noeud1, noeud2) ",12,noeud1,noeud2)
-
-"""
-mutable struct comp_conx{T,S} <: Abstractcomp_conx{T}
+mutable struct comp_conx{T} <: Abstractcomp_conx{T}
   name::String
   Child::Node{T}
   Parent::Node{T}
-  
- 
 end
 
 # on présume que tous les noeuds dérivant d'AbstractNode
@@ -31,9 +20,14 @@ name(comp_conx::Abstractcomp_conx) = comp_conx.name
 """Renvoie les données contenues dans le noeud."""
 data(comp_conx::Abstractcomp_conx) = comp_conx.parent
 
+function comp_conx(Node::Node{T}) where {T}
+name=Node.name  
+return comp_conx(name,Node,Node)
+end
+
 function change_parent!(comp_conx::Abstractcomp_conx, new_parent::Node{T}) where {T}
-    comp_conx.parent=new_paren
-  end
+    comp_conx.parent=new_parent
+end
 
 
 
