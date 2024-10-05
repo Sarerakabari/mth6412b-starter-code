5cee10229d70946c4b0496f6a53b202000c743d8
Mouhtal Oussama <mouhtal@Omnissiah-MacBookPro-Algo.local>
2024-10-05 12:43:15 -0400

    changes

--- src/phase1/comp_connexe.jl
+++ src/phase1/comp_connexe.jl
@@ -2,7 +2,7 @@ import Base.show
 include("graph.jl")
 
 
-"""Type abstrait dont d'autres types d'arret dériveront."""
+"""Type abstrait dont d'autres types de composantes connexe dériveront."""
 abstract type Abstractcomp_conx{T} end
 
 mutable struct comp_conx{T} <: Abstractcomp_conx{T}
@@ -11,22 +11,21 @@ mutable struct comp_conx{T} <: Abstractcomp_conx{T}
   Parent::Node{T}
 end
 
-# on présume que tous les noeuds dérivant d'AbstractNode
-# posséderont des champs `name` et `data`.
+# on présume que tous les composantes d'un graphes dérivant d'Abstractcomp_conx
+# posséderont des champs `name` et `child` et `Parent`.
 
-"""Renvoie le nom du noeud."""
+"""Renvoie le nom de la composante connexe."""
 name(comp_conx::Abstractcomp_conx) = comp_conx.name
 
-"""Renvoie les données contenues dans le noeud."""
 data(comp_conx::Abstractcomp_conx) = comp_conx.parent
 
 function comp_conx(Node::Node{T}) where {T}
-name=Node.name  
-return comp_conx(name,Node,Node)
+  name=Node.name  
+  return comp_conx(name,Node,Node)
 end
 
 function change_parent!(comp_conx::Abstractcomp_conx, new_parent::Node{T}) where {T}
-    comp_conx.parent=new_parent
+  comp_conx.parent=new_parent
 end
 
 
