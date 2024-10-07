0579f626395d6cc9d6b038015dcce76c2d6bffb2
Oussamamouhtal <oussamamouhtal22@gmail.com>
2024-10-07 19:51:36 -0400

    commentaires additionel

--- src/phase2/node_pointer.jl
+++ src/phase2/node_pointer.jl
@@ -9,30 +9,16 @@ mutable struct node_pointer{T} <: Abstractnode_pointer{T}
   name::String
   child::Node{T}
   parent::Node{T}
-
 end
 
-
-"""Constructeur pour un composant connexe."""
+"""Constructeur d'une composante connexe à partir d'un noeud"""
 function node_pointer(Node::Node{T}) where {T}
-name=Node.name 
-return node_pointer(name,Node,Node)
-end
-""" Union des composants connexes composés des 2 noeuds des arête d'un graph (union des racines) """
-function unite!(n1::Node{T},n2::Node{T},C::Vector{node_pointer{T}}) where {T}
-
-  link!(find_root(C[findfirst(x->x.name==n1.name,C)],C),find_root(C[findfirst(x->x.name==n2.name,C)],C),C)
-  C
-end    
-
-""" liason 2 composants connexes dans un ensemble """
-function link!(c1::node_pointer{T},c2::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
-
-  C[findfirst(x->x.name==c2.name,C)].parent=c1.child
-  C
+  name=Node.name 
+  return node_pointer(name,Node,Node)
 end
 
-""" Trouve la racine du composant connexe """
+""" find_root serve pour trouver la racine dans un ensemble de composantes connexes. 
+Une racine est déinit par une composante connexe dont l'enfant est lui meme le parent. """
 
 function find_root(c::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
 
@@ -42,3 +28,24 @@ function find_root(c::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
 
   return c
 end
+
+
+""" liaison de deux composantes connexes dans un ensemble de composantes connexes"""
+function link!(c1::node_pointer{T},c2::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
+
+  C[findfirst(x->x.name==c2.name,C)].parent=c1.child
+  C
+end
+
+
+
+""" Liaison de deux composantes connexes qui sont les racines des neuds donées en argument
+dans un ensemble de composantes connexes"""
+function unite!(n1::Node{T},n2::Node{T},C::Vector{node_pointer{T}}) where {T}
+
+  link!(find_root(C[findfirst(x->x.name==n1.name,C)],C),find_root(C[findfirst(x->x.name==n2.name,C)],C),C)
+  C
+end    
+
+
+
