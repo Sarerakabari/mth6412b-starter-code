d3f52dbd58b00032fb8da612117688a1ce5a9d9f
Ando Rakotonandrasana <ando-sitraka-2.rakotonandrasana@polymtl.ca>
2024-10-28 13:54:48 -0400

    main

--- src/phase3/main.jl
+++ src/phase3/main.jl
@@ -10,13 +10,21 @@ include("prim.jl")
 G=create_graph("../../instances/stsp/bays29.tsp")
 
 #Test sur le fichier bayg29.tsp
+
+#Kruskal avec heuristique
 A,B=kruskal(G)
 
 println("the minimun spanning tree are composed of:")
 for a in A
     show(a)
 end
+
 println("the total cost is ",B)
+
+
+#prim
+
 C,D=prim(G)
 
 show(C)
+println("the total cost is ",D)
