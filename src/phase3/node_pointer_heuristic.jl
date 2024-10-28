9a80fffd67ea5c69c6a9ed6c87b443fd2e066f56
oclaich <octave.claich@gmail.com>
2024-10-28 15:30:30 -0400

    Update node_pointer_heuristic.jl
    Ajout documentation

--- src/phase3/node_pointer_heuristic.jl
+++ src/phase3/node_pointer_heuristic.jl
@@ -5,6 +5,8 @@ include("../phase1/graph.jl")
 """Type abstrait d'un composant connexe: noeud enfant pointant vers le noeud parent ."""
 abstract type Abstractnode_pointer{T} end
 
+"""Type node_pointer (composant connexe) contenant 4 attributs : son nom (de type String), le noeud enfant (de type Node{T})
+qui est le noeud à proprement parler, le noeud parent (de type Node{T}), et son rang (de type Int64)."""
 mutable struct node_pointer{T} <: Abstractnode_pointer{T}
   name::String
   child::Node{T}
@@ -18,8 +20,8 @@ function node_pointer(Node::Node{T}) where {T}
   return node_pointer(name,Node,Node,0)
 end
 
-""" find_root serve pour trouver la racine dans un ensemble de composantes connexes. 
-Une racine est déinit par une composante connexe dont l'enfant est lui meme le parent. """
+""" find_root sert à trouver la racine dans un ensemble de composantes connexes. 
+Une racine est déinie par une composante connexe dont l'enfant est lui-même le parent. """
 
 function find_root(c::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
 
@@ -31,7 +33,8 @@ function find_root(c::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
 end
 
 
-""" liaison de deux composantes connexes dans un ensemble de composantes connexes avec le rang"""
+""" Liaison de deux composantes connexes dans un ensemble de composantes connexes en utilisant
+l'heuristique d'union par le rang. """
 function link!(c1::node_pointer{T},c2::node_pointer{T},C::Vector{node_pointer{T}}) where {T}
 
   if c1.rank > c2.rank
@@ -47,7 +50,7 @@ end
 
 
 
-""" Liaison par compression de chemin"""
+""" Liaison en utilisant l'heuristique de compression de chemins."""
 function unite!(n1::Node{T},n2::Node{T},C::Vector{node_pointer{T}}) where {T}
 
   link!(find_root(C[findfirst(x->x.name==n1.name,C)],C),find_root(C[findfirst(x->x.name==n2.name,C)],C),C)
