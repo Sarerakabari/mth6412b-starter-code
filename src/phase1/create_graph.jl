65bec6907a68e97db9620a4bb9ea7e272db54804
Octave Claich <octave.claich@gmail.com>
2024-11-13 17:52:51 -0500

    Create_graph remis à jour

--- src/phase1/create_graph.jl
+++ src/phase1/create_graph.jl
@@ -42,4 +42,4 @@ end
 return graph=Graph(header["NAME"],nodes_vec,edges_vec)
 end
 
-# create_graph("C:/Users/Ando/Desktop/mth6412b-starter-code/instances/stsp/swiss42.tsp")
+create_graph("C:/Users/Ando/Desktop/mth6412b-starter-code/instances/stsp/swiss42.tsp")
