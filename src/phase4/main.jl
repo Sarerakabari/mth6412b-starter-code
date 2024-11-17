649dab7a1f27530f25cb9c022a261b6fd9ebf14c
Oussamamouhtal <oussamamouhtal22@gmail.com>
2024-11-17 17:22:38 -0500

    add main file

--- src/phase4/main.jl
+++ src/phase4/main.jl
@@ -7,15 +7,40 @@ include("hk.jl")
 include("finetuning.jl")
 include("visualise_graph.jl")
 
-#list_eps = [1e-1,2*1e-1, 3*1e-1, 4*1e-1, 5*1e-1,6*1e-1,7*1e-1, 8*1e-1, 9*1e-1,1e-2,1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
+list_eps = [1e-1,2*1e-1, 3*1e-1, 4*1e-1, 5*1e-1,6*1e-1,7*1e-1, 8*1e-1, 9*1e-1,1e-2,1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
 
-T1,C1,ID1 = finetuning_start_rsl("/Users/mouhtal/Desktop/mth6412b-starter-code-5/instances/stsp/bayg29.tsp")
-#T2,C2,EPS2 = finetuning_epsilon_hk("/Users/mouhtal/Desktop/mth6412b-starter-code-5/instances/stsp/bayg29.tsp", 1,list_eps)
-#T3,C3,ID3 = finetuning_start_hk("/Users/mouhtal/Desktop/mth6412b-starter-code-5/instances/stsp/bayg29.tsp", EPS)
-#T4,C4,EPS4,ID4 = finetuning_start_epsilon_hk("/Users/mouhtal/Desktop/mth6412b-starter-code-5/instances/stsp/bayg29.tsp", list_eps)
-println(C1)
-show(T1)
-visualize_graph(T1.Nodes,T1.Edges)
-#println(C2)
-#println(C3)
-#println(C4)
+
+""" Afiichage des résultats """
+function main(filename::String, finetunning::String)
+
+    if finetunning == "start_rsl"
+        T, C, ID =  finetuning_start_rsl(filename)
+        println("Le coût de la tournée est : ", C)
+        println("La tournée est composée par :")
+        show(T)
+        visualize_graph(T.Nodes,T.Edges)
+
+    elseif finetunning == "start_hk"
+        T, C, ID =  finetuning_start_hk(filename, 1e-1)
+        println("Le coût de la tournée est", C)
+        println("La tournée est composée par:")
+        show(T)
+        visualize_graph(T.Nodes,T.Edges)
+    elseif finetunning == "epsilon_hk"
+        T, C, EPS =  finetuning_epsilon_hk(filename,1,list_eps)
+        println("Le coût de la tournée est", C)
+        println("La tournée est composée par:")
+        show(T)
+        visualize_graph(T.Nodes,T.Edges)
+    else finetunning == "start_epsilon_hk"
+        T, C, ID =  finetuning_start_epsilon_hk(filename,list_eps)
+        println("Le coût de la tournée est :  ", C)
+        println("La tournée est composée par:")
+        show(T)
+        visualize_graph(T.Nodes,T.Edges)
+    end
+
+end
+
+
+main("/Users/mouhtal/Desktop/mth6412b-starter-code-5/instances/stsp/bayg29.tsp", "start_rsl")
