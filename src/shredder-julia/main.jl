d6911699c9205781c8f6286b467e9989830f48f2
Octave Claich <octave.claich@gmail.com>
2024-12-14 14:28:22 -0500

    Modifications dans le main shredder-julia, problème sur la fonction reconstruct_picture

--- src/shredder-julia/main.jl
+++ src/shredder-julia/main.jl
@@ -2,7 +2,20 @@ include("../phase1/node.jl")
 include("../phase1/Edge.jl")
 include("../phase1/graph.jl")
 include("../phase1/read_stsp.jl")
+include("../phase1/create_graph.jl")
+include("../phase2/node_pointer.jl")
+include("../phase2/kruskal.jl")
+include("../phase3/node_priority.jl")
+include("../phase3/queue.jl")
+include("../phase3/prim.jl")
 include("../phase4/rsl.jl")
+include("../phase4/degrees.jl")
+include("../phase4/weighted_node.jl")
+include("../phase4/weigth_update.jl")
+include("../phase4/sub_graph.jl")
+include("../phase4/fix_tree.jl")
+include("../phase4/hk.jl")
+include("../phase4/finetuning.jl")
 
 # Chemin du dossier contenant les fichiers .tsp
 tsp_folder = joinpath(dirname(@__FILE__), "tsp", "instances")
@@ -13,12 +26,12 @@ for file in readdir(tsp_folder)
         filepath = joinpath(tsp_folder, file)
         filename = splitext(basename(filepath))[1]
 
-        header=read_header(filepath)
-        nodes=read_nodes(header,filepath)  # On récupère les noeuds du tsp
-        edges=read_edges(header,filepath) # On récupère les arêtes du tsp
-        graph=Graph(filename,nodes,edges) # Création du graph correspondant au tsp
+        graph=create_graph(filepath)
 
         tournee,weight=rsl(graph,1) # Utilisation de l'algorithme rsl pour trouver une tournée minimale
+        tournee=tournee.Nodes
+        weight=Float32(weight)
+        node_ids = [parse(Int, node.name) for node in tournee]
 
         write_tour(filename,tournee,weight) # Ecriture du fichier .tour de la tournée correspondante
 
